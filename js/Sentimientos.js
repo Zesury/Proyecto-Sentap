7ba05ffaa0c3cf87c10d9fd0e4d7d96266d061bc
Zesury <Carlo.1.pdro@gmail.com>
2023-10-11 19:05:03 -0600

    first commit

---
+++ js/Sentimientos.js
@@ -0,0 +1,118 @@
+var player1;
+var player2;
+var player3;
+var player4;
+var player5;
+var player6;
+var player7;
+var player8;
+var player9;
+var player10;
+var player11;
+var player12;
+var player13;
+var player14;
+var player15;
+var seleccion = document.getElementById("Emociones")
+
+
+alert("Debes saber que esta pagina te muestra cosas relacionadas con como te sientas, si te sintes feliz te muestra cosas felices :D")
+
+function onYouTubeIframeAPIReady() {
+    player1 = new YT.Player('player1',
+        {
+            height: '225',
+            width: '400',
+            videoId: 'h3krVQ9Fq1M'
+        });
+    player2 = new YT.Player('player2',
+        {
+            height: '225',
+            width: '400',
+            videoId: 'Ld0NDDYXj-c'
+        });
+    player3 = new YT.Player('player3',
+        {
+            height: '225',
+            width: '400',
+            videoId: 'FmZRu4VkV44'
+        });
+    player4 = new YT.Player('player4',
+        {
+            height: '225',
+            width: '400',
+            videoId: 'M29e_4-EyH4'
+        });
+    player5 = new YT.Player('player5',
+        {
+            height: '225',
+            width: '400',
+            videoId: 'UJ0I5m3OnL0'
+        })
+        ; player6 = new YT.Player('player6',
+            {
+                height: '225',
+                width: '400',
+                videoId: 'jMy5t9upRvQ'
+            });
+    player7 = new YT.Player('player7',
+        {
+            height: '225',
+            width: '400',
+            videoId: 'jv0y64Nsb9o'
+        });
+    player8 = new YT.Player('player8',
+        {
+            height: '225',
+            width: '400',
+            videoId: 'JSBPnRbHXvs'
+        })
+        ; player9 = new YT.Player('player9',
+            {
+                height: '225',
+                width: '400',
+                videoId: '2mVmGVORA-o'
+            });
+    player10 = new YT.Player('player10',
+        {
+            height: '225',
+            width: '400',
+            videoId: 'XRWAhy1mVnI'
+        });
+    player11 = new YT.Player('player11',
+        {
+            height: '225',
+            width: '400',
+            videoId: 'WV-FvN1U1yI'
+        });
+    player12 = new YT.Player('player12',
+        {
+            height: '225',
+            width: '400',
+            videoId: 'rrGXBfmbuIc'
+        });
+    player13 = new YT.Player('player13',
+        {
+            height: '225',
+            width: '400',
+            videoId: '9Jpfh2kUWBs'
+        });
+    player14 = new YT.Player('player14',
+        {
+            height: '225',
+            width: '400',
+            videoId: 'jfKfPfyJRdk'
+        });
+    player15 = new YT.Player('player15',
+        {
+            height: '225',
+            width: '400',
+            videoId: '4xDzrJKXOOY'
+        });
+}
+
+
+// function seleccionDeEmocion
+// {
+//     if 
+// }
